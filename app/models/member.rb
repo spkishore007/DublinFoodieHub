8555dba5b8e11214fcd42f66a77ff953f1aea0c2
subitb <X16118286@student.ncirl.ie>
2017-03-25 15:46:27 +0000

    Added brute force security features

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,5 +3,15 @@ class Member < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :validatable
+         :recoverable, :trackable, :validatable, password_length: 9..128
+         
+   validate :strong_password_check
+   
+   validates :user_name, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z]+([a-zA-Z]|\d)*\Z/ }
+
+  def strong_password_check
+    if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)./)
+      errors.add "Your password must include at least one digit, one lowercase and uppercase letter"
+    end
+  end
 end
