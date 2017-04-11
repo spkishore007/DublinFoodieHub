59c6e9d1e121a32cdee4e44e5336fec80797fca7
Kishore Kumar <x16103602@student.ncirl.ie>
2017-04-11 15:33:42 +0000

    change in observer function

--- app/models/order_observer.rb
+++ app/models/order_observer.rb
@@ -1,3 +1,5 @@
+require 'product_update_single'
+require 'pricing_observer'
 class OrderObserver
   def update(record)
       
@@ -7,6 +9,29 @@ class OrderObserver
      	record.order_items.each do |order_item|
          order_item.product.update_attribute(:quantityAvailable,order_item.product.quantityAvailable.to_i - order_item.quantity) 
         end
+      product_count_update
+      price_change
     end
   end
+  
+  def product_count_update
+    @product_count = ProductUpdateSingleton.instance
+    @count_up = @product_count.count_update
+    puts(@count_up)
+  end
+  
+  def product_refresh_qty
+    @refresh = ProductUpdateSingleton.instance
+    @refresh_qty = @refresh.refresh_quantity
+    puts(@refresh_qty)
+  end
+  
+  def price_change
+    order = OrderSend.new()
+    result = Pricing.new(order)
+    puts(result)
+    order.notify_observers
+    puts("Pricing is completed. =========================================== Now count update and hit update is going to perform")
+  end
+  
 end
